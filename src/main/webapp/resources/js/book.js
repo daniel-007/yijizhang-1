6c0d15535dd57ad928d545ff3c64cde7772e9239
john <john@jhu>
2015-09-24 10:51:43 +0800

    去掉alert

--- src/main/webapp/resources/js/book.js
+++ src/main/webapp/resources/js/book.js
@@ -4,7 +4,6 @@
 $(function () {
 
     var $btn_xjzt = $("#btn_xjzt");
-    alert(1);
     $btn_xjzt.click(function () {
         $("#default_win").window({
             title: '<i class="fa fa-list-alt"></i>新建账套',
