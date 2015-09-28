de07317096bb79959924cf4ba168a8e0a8e1592b
Joey Yan <447002290@qq.com>
2015-09-28 17:06:39 +0800

    update

--- src/main/java/cn/ahyc/yjz/config/SecurityConfiguration.java
+++ src/main/java/cn/ahyc/yjz/config/SecurityConfiguration.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
