86648738baae184768b1f536e4f90e0291248d0e
sanlli <sanlli@cisco.com>
2015-10-19 19:22:42 +0800

    修改注释

--- src/main/java/cn/ahyc/yjz/controller/AppController.java
+++ src/main/java/cn/ahyc/yjz/controller/AppController.java
@@ -37,7 +37,6 @@ import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * AppController
