84f94918b2c52b511d9c1bd57c1fc678f875f14c
sanlli <sanlli@cisco.com>
2015-10-22 14:45:27 +0800

    补充一些业务操作日志

--- src/main/java/cn/ahyc/yjz/aop/VoucherControllerActlog.java
+++ src/main/java/cn/ahyc/yjz/aop/VoucherControllerActlog.java
@@ -40,6 +40,42 @@ public class VoucherControllerActlog extends ActionLogAopConfiguration{
 				String method = method(point);
 				ActionLog actionLog = null;
 				switch (method){
+						case("voucher"):
+								actionLog = actionLog("打开记账页面","开始","",point);
+								break;
+						case("voucherDetailList"):
+								actionLog = actionLog("查看凭证明细列表","开始","",point);
+								break;
+						case("save"):
+								actionLog = actionLog("保存凭证","开始","",point);
+								break;
+						case("help"):
+								actionLog = actionLog("查看凭证制作说明","开始","",point);
+								break;
+						case("subjectBalance"):
+								actionLog = actionLog("打开记账-科目余额页面","开始","",point);
+								break;
+						case("voucherTemplate"):
+								actionLog = actionLog("打开模式凭证列表页面","开始","",point);
+								break;
+						case("voucherTemplateList"):
+								actionLog = actionLog("查看模式凭证列表","开始","",point);
+								break;
+						case("voucherTemplateAdd"):
+								actionLog = actionLog("打开模式凭证新增/编辑页面","开始","",point);
+								break;
+						case("voucherTemplateSave"):
+								actionLog = actionLog("打开模式凭证保存页面","开始","",point);
+								break;
+						case("voucherTemplateDetailList"):
+								actionLog = actionLog("查看模式凭证明细列表","开始","",point);
+								break;
+						case("deleteTemplate"):
+								actionLog = actionLog("删除模式凭证","开始","",point);
+								break;
+						case("saveTemplate"):
+								actionLog = actionLog("保存模式凭证","开始","",point);
+								break;
 						default:
 								break;
 				}
@@ -56,6 +92,42 @@ public class VoucherControllerActlog extends ActionLogAopConfiguration{
 				String method = method(point);
 				ActionLog actionLog = null;
 				switch (method){
+						case("voucher"):
+								actionLog = actionLog("打开记账页面","结束","",point);
+								break;
+						case("voucherDetailList"):
+								actionLog = actionLog("查看凭证明细列表","结束","",point);
+								break;
+						case("save"):
+								actionLog = actionLog("保存凭证","结束","",point);
+								break;
+						case("help"):
+								actionLog = actionLog("查看凭证制作说明","结束","",point);
+								break;
+						case("subjectBalance"):
+								actionLog = actionLog("打开记账-科目余额页面","结束","",point);
+								break;
+						case("voucherTemplate"):
+								actionLog = actionLog("打开模式凭证列表页面","结束","",point);
+								break;
+						case("voucherTemplateList"):
+								actionLog = actionLog("查看模式凭证列表","结束","",point);
+								break;
+						case("voucherTemplateAdd"):
+								actionLog = actionLog("打开模式凭证新增/编辑页面","结束","",point);
+								break;
+						case("voucherTemplateSave"):
+								actionLog = actionLog("打开模式凭证保存页面","结束","",point);
+								break;
+						case("voucherTemplateDetailList"):
+								actionLog = actionLog("查看模式凭证明细列表","结束","",point);
+								break;
+						case("deleteTemplate"):
+								actionLog = actionLog("删除模式凭证","结束","",point);
+								break;
+						case("saveTemplate"):
+								actionLog = actionLog("保存模式凭证","结束","",point);
+								break;
 						default:
 								break;
 				}
