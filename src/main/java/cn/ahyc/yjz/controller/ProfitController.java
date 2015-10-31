67a2fdb1ca2cfcc7e764b4babfe50a5d6fc3108a
chengjiarui <1256064203@qq.com>
2015-10-31 23:26:11 +0800

    利润表计算bug修改、页面修改公式实时计算、页面卡死bug修改

--- src/main/java/cn/ahyc/yjz/controller/ProfitController.java
+++ src/main/java/cn/ahyc/yjz/controller/ProfitController.java
@@ -3,19 +3,24 @@
  */
 package cn.ahyc.yjz.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.ahyc.yjz.dto.ExpressionColumn;
 import cn.ahyc.yjz.model.Period;
 import cn.ahyc.yjz.service.ProfitService;
 import cn.ahyc.yjz.util.Constant;
@@ -60,9 +65,10 @@ public class ProfitController extends BaseController {
      * @param searchtPeriod
      * @return
      */
-    @RequestMapping("/list")
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String, Object> list(Model model, HttpSession session, Integer searchtPeriod) {
+    public Map<String, Object> list(Model model, HttpSession session, Integer searchtPeriod,
+            HttpServletRequest request) {
         
         Period period = (Period) session.getAttribute(Constant.CURRENT_PERIOD);
         Long bookId = period.getBookId();
@@ -72,42 +78,78 @@ public class ProfitController extends BaseController {
         } else {
             currentPeriod = period.getCurrentPeriod();
         }
-        List<Map<String, Object>> list = profitService.getList(currentPeriod, bookId);
+        List<Map<String, Object>> list;
+        List<ExpressionColumn> expList = new ArrayList<ExpressionColumn>();
+        String[] cAs = getValues("cA", request);
+        String[] cBs = getValues("cB", request);
+        String[] cCs = getValues("cC", request);
+        String[] cAVals = getValues("cAVal", request);
+        String[] cBVals = getValues("cBVal", request);
+        String[] cCVals = getValues("cCVal", request);
+        if (cAs != null) {
+            ExpressionColumn entry;
+            int i = 0;
+            for (String cA : cAs) {
+                entry = new ExpressionColumn();
+                entry.setcA(cA);
+                entry.setcAVal(cAVals[i]);
+                entry.setcB(cBs[i]);
+                entry.setcBVal(cBVals[i]);
+                entry.setcC(cCs[i]);
+                entry.setcCVal(cCVals[i]);
+                i++;
+            }
+        }
+        if (expList != null && expList.size() > 0) {
+            list = profitService.getListWithExpList(expList, currentPeriod, bookId);
+        } else {
+            list = profitService.getList(currentPeriod, bookId);
+        }
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("total", list != null && list.size() > 0 ? list.size() : 0);
         map.put("rows", list);
         return map;
     }
 
+    private String[] getValues(String name, HttpServletRequest request) {
+        String str = request.getParameter(name);
+        if (StringUtils.isBlank(str)) {
+            return null;
+        }
+        String[] values = str.split(",", -1);
+        return values;
+    }
+
     /**
-     * 公式计算
+     * 数据列表
      * 
      * @param model
      * @param session
      * @param searchtPeriod
-     * @param expStr
      * @return
      */
-    @RequestMapping("/count")
+    @RequestMapping(value = "/count", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String, Object> count(Model model, HttpSession session, Integer searchtPeriod, String expStr) {
-        Map<String, Object> map = new HashMap<String, Object>();
-        try {
-            if (StringUtils.isNotBlank(expStr)) {
-                Period period = (Period) session.getAttribute(Constant.CURRENT_PERIOD);
-                Long bookId = period.getBookId();
-                Integer currentPeriod;
-                if (searchtPeriod != null) {
-                    currentPeriod = searchtPeriod;
-                } else {
-                    currentPeriod = period.getCurrentPeriod();
-                }
-                map.put("success", profitService.countExp(currentPeriod, bookId, expStr));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            map.put("msg", e.getMessage());
+    public Map<String, Object> count(Model model, HttpSession session, Integer searchtPeriod,
+            @RequestBody(required = false) List<ExpressionColumn> expList) {
+
+        Period period = (Period) session.getAttribute(Constant.CURRENT_PERIOD);
+        Long bookId = period.getBookId();
+        Integer currentPeriod;
+        if (searchtPeriod != null) {
+            currentPeriod = searchtPeriod;
+        } else {
+            currentPeriod = period.getCurrentPeriod();
         }
+        List<Map<String, Object>> list;
+        if (expList != null) {
+            list = profitService.getListWithExpList(expList, currentPeriod, bookId);
+        } else {
+            list = profitService.getList(currentPeriod, bookId);
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("total", list != null && list.size() > 0 ? list.size() : 0);
+        map.put("rows", list);
         return map;
     }
 }
--- src/main/resources/static/js/profit.js
+++ src/main/resources/static/js/profit.js
@@ -8,6 +8,15 @@ Profit=function(){
 		
 		//初始化
 		init:function(searchtPeriod) {
+			$('#profitSave').click(function() {
+				Voucher.add();
+			});
+			$('#profitAppend').click(function() {
+				Voucher.add();
+			});
+			$('#profitRemoveit').click(function() {
+				Voucher.add();
+			});
 			
 			$('#searchtPeriod').numberspinner({
 				min:1,
@@ -18,58 +27,66 @@ Profit=function(){
 				}
 			});
 			
+			Profit.datainit(1);
+		},
+		datainit:function(flag){
 			//表格
 			var dg = $('#profitDg').datagrid({
 				singleSelect:true,
-				fitColumns: true,
-				fit:true,
+				//fitColumns: true,
+				//fit:true,
 				rownumbers:true,
 				clickToEdit: false,
 		        dblclickToEdit: true,
-				toolbar: '#profitDgTd',
+				toolbar: '#profitMenu,#profitDgTd',
 				url:'profit/list',
-				method:'get',
 				columns:[[
-							{field:'cA',title:'A',width:60,align:'left',halign:'center'},
-							{field:'cB',title:'B',width:40,align:'right',halign:'center',
+							{field:'cA',title:'A',width:260,align:'left',halign:'center'},
+							{field:'cB',title:'B',width:240,align:'right',halign:'center',
 								formatter: function(value,row,index){
 									if(index==0){
-										return '<div style="text-align: center;">'+row.cBVal+'</div>';
+										return '<div style="text-align: center;">'+row.cB+'</div>';
 									} else {
-										return row.cBVal==0?'':Profit.formatCurrency(row.cBVal);
+										return row.cBVal;
 						            }
 								},
 								editor:'textbox'
 							},
-							{field:'cC',title:'C',width:40,align:'right',halign:'center',
+							{field:'cC',title:'C',width:240,align:'right',halign:'center',
 								formatter: function(value,row,index){
 									if(index==0){
-										return '<div style="text-align: center;">'+row.cCVal+'</div>';
+										return '<div style="text-align: center;">'+row.cC+'</div>';
 									} else {
-										return row.cCVal==0?'':Profit.formatCurrency(row.cCVal);
+										return row.cCVal;
 						            }
 								},
 								editor:'textbox'
 							}
 						]],
 				onBeforeLoad:function(param){
+//					var rows=$('#profitDg').datagrid('getRows');
+//					if(rows){
+//						for(i=0;i<rows.length;++i){
+//							param=Profit.extend(param,JSON.parse(JSON.stringify(rows[i]).replace(/(\[.+\])/g,'$1')));
+//						}
+//					}
 					param.searchtPeriod=$('#searchtPeriod').numberspinner('getValue');
 				},
 				onLoadSuccess:function(data) {
-					$(this).datagrid('enableCellEditing', function (cur_idx, pre_idx, field) {
-                        //可修改判断
-						if(cur_idx==0){
-							return false;
-						}else{
-							return true;
-						}
-					});
+					if(flag){
+						$('#profitDg').datagrid('enableCellEditing', function (cur_idx, pre_idx, field) {
+	                        //可修改判断
+							if(cur_idx==0){
+								return false;
+							}else{
+								return true;
+							}
+						});
+						flag = undefined;
+					}
 				},
 				onEndEdit:function(index,row,changes){
-					if(!changes){
-						return;
-					}
-					var cell = $(this).datagrid('cell');
+					var cell =$('#profitDg').datagrid('cell');
 					if(!cell){
 						return;
 					}
@@ -78,55 +95,76 @@ Profit=function(){
 					if(!expStr){
 						return;
 					}
-			        for(i=1;i<rows.length;i++){
-			        	expStr=expStr.replace(new RegExp('(B'+(i+1)+')([^0-9]?)','g'),rows[i].cBVal+'$2');
-			        	expStr=expStr.replace(new RegExp('(C'+(i+1)+')([^0-9]?)','g'),rows[i].cCVal+'$2');
-			        }
+					rows[cell.index][cell.field]=expStr;
 			        console.log("count expression:"+expStr);
 			        //计算公式
-					if(expStr){
-						var searchtPeriod=$('#searchtPeriod').numberspinner('getValue');
-						$.ajax({
-                            url: 'profit/count',
-                            data: {expStr: expStr,searchtPeriod: searchtPeriod},
-                            type: 'POST',
-                            async: true,
-                            success: function (data) {
-                            	var oldValue = rows[cell.index][cell.field+'Val'];
-                                if (data.success||data.success==0) {
-                                	rows[cell.index][cell.field+'Val']=data.success;
-                                	$('#profitDg').datagrid('updateRow',{
-                                		index: cell.index
-                                	});
-                                	var exp = new RegExp('('+cell.field.replace("c",'')+cell.index+')([^0-9]?)','g');
-                                	for(i=1;i<rows.length;i++){
-                                		if(rows[i].cB.indexOf(exp)!=-1){
-                                			rows[i].cBVal=Profit.sum(rows[i].cB,rows[i].cBVal,exp,oldValue,data.success);
-                                			$('#profitDg').datagrid('updateRow',{index: i});
-                                		}
-                                		if(rows[i].cC.indexOf(exp)!=-1){
-                                			rows[i].cCVal=Profit.sum(rows[i].cC,rows[i].cCVal,exp,oldValue,data.success);
-                                			$('#profitDg').datagrid('updateRow',{index: i});
-                                		}
-                                	}
-                                } else {
-                                	$.messager.alert("错误", data.msg, "error");
-                                	rows[cell.index][cell.field+'Val']=oldValue;
-                                	$('#profitDg').datagrid('updateRow',{
-                                		index: cell.index
-                                	});
-                                }
+			        var searchtPeriod=$('#searchtPeriod').numberspinner('getValue');
+			        //$('#profitDg').datagrid('reload');
+					$.ajax({
+                        url: 'profit/count',
+                        data: JSON.stringify(rows),
+                        type: 'POST',
+                        dataType:"json",      
+                        contentType:"application/json",
+                        async: true,
+                        success: function (data) {
+                        	console.log(data);
+                            if (data) {
+                            	$('#profitDg').datagrid('loadData', data);
+                            } else {
+                            	$.messager.alert("错误", data.msg, "error");
                             }
-                        });
-					}
+                        }
+                    });
 				},
 				onClickCell:function(){
-					var cell = $(this).datagrid('cell');
+					var cell = $('#profitDg').datagrid('cell');
 					if(cell){
-						$(this).datagrid('endEdit', cell.index);
+						$('#profitDg').datagrid('endEdit', cell.index);
 					}
 				}
 			});
+		},
+		extend:function(src, override){
+			for(var i in override){
+				if(!override[i]||override[i]=='null'){
+					override[i] = '';
+				}
+				if(i in src){
+					src[i] = src[i]+','+override[i];
+				} else {
+					src[i] = override[i];
+				}
+			} 
+			return src;
+		},
+		//表格修改数据
+		getChanges:function(){
+			var params='';
+			var rows = $('#profitDg').datagrid('getRows');
+			for(i=0;i<rows.length;++i){
+				if(rows[i].subjectCode){
+					params+='&'+JSON.stringify(rows[i]);
+				}
+			}
+			params=params.replace(/{/g,'').replace(/}/g,'').replace(/","/g,'&').replace(/"/g,'').replace(/:/g,'=');
+			params=params.replace(/,/g,'&').replace(/undefined/g,'');
+			return params;
+		},
+		updateRows:function(){
+			var exp = new RegExp(cell.field.replace("c",'')+cell.index+'([^0-9])?','g');
+        	console.log(exp);
+        	console.log(exp.test('=B2+B3+B4'));
+        	for(i=1;i<rows.length;i++){
+        		if(exp.test(rows[i].cB)){
+        			rows[i].cBVal=Profit.sum(rows[i].cB,rows[i].cBVal,exp,oldValue,data.success);
+        			$('#profitDg').datagrid('updateRow',{index: i});
+        		}
+        		if(exp.test(rows[i].cC)){
+        			rows[i].cCVal=Profit.sum(rows[i].cC,rows[i].cCVal,exp,oldValue,data.success);
+        			$('#profitDg').datagrid('updateRow',{index: i});
+        		}
+        	}
 		},
 		sum:function(cExp,cVal,exp,oldValue,newValue){
 			var dir = cExp.substr(cExp.indexOf(exp),1);
